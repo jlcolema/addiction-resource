0f537bbf648a33ec6fd78b3d83ed65b653c97273
Joshua Coleman <4628855+jlcolema@users.noreply.github.com>
2022-07-28 09:15:41 -0500

    Copy starting structure from blog landing page.

---
+++ app/public/wp-content/themes/RehabPanther/page-guide-page.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+Template Name: Guide Page
+*/
+
+get_header();?>
+
+<div class="page-wrapper">
+
+	<!-- Intro -->
+
+	<div class="pageIntro white-intro">
+
+		<div class="container">
+
+			<div class="row">
+			
+				<div class="col-md-8 col-12">
+		  
+					<h1 class="subpageTitle" data-ix="fade-in-on-load" itemprop="headline">Guide Page</h1>
+		  
+					<div class="subpage-subtitle">
+
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci quam morbi sagittis non amet, metus, curabitur. Sed placerat in elementum curabitur consequat tellus venenatis venenatis. Consectetur eget iaculis amet, cras leo eget. Egestas molestie proin egestas rhoncus ac nibh sit dignissim adipiscing.</p>
+
+					</div>
+			
+					<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb( ' <div id="breadcrumbs">','</div>' );}?>
+			
+				</div>
+		
+			</div>
+		
+		</div>
+	
+	</div>
+	
+	<!-- Listing -->
+
+	<div class="container">
+
+		<div class="row content no-pad">
+	
+			<div class="col-md-9 col-12">
+		
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				
+					<?php if (have_posts()) : ?>
+					
+						<ol class="ar-blog">
+						
+							<?php while (have_posts()) : the_post(); ?>
+							
+								<li class="br-list-item">
+									
+									<figure class="br-fig">
+									
+										<a href="<?php the_permalink(); ?>">
+										
+											<picture>
+											
+												<?php the_post_thumbnail('custom-blog'); ?>
+											
+											</picture>
+										
+										</a>
+									
+									</figure>
+									
+									<div class="br-blck">
+									
+									<h2 class="br-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									
+										<p class="br-sub-title"><a href="<?php the_permalink(); ?>"><?php echo excerpt('20');?></a></p>
+									
+										<a class="brarw"  href="<?php the_permalink(); ?>">READ MORE <i class="fal fa-arrow-right"></i></a>
+									
+									</div>
+							
+								</li>
+					
+							<?php endwhile; ?>
+						
+						</ol>
+				
+					<?php endif; ?>
+				
+					<div class="clearfix pagination-wrap">
+				
+						<ul class="pagination">
+					  	
+							<?php
+								global $wp_query;
+								$big = 999999999; // need an unlikely integer
+								echo paginate_links( array(
+									'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+									'format' => '?paged=%#%',
+									'current' => max( 1, get_query_var('paged') ),
+									'total' => $wp_query->max_num_pages
+						  		));
+							?>
+					
+						</ul>
+				
+					</div>
+			
+				</article>
+		
+			</div>
+
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
